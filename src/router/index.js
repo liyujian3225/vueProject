aef63b2c36b3d619a0b772cb329323e7016b20cb
liyujian <icanflyanywhere@126.com>
2021-03-30 09:15:07 +0800

    路由跳转页面title设置

--- src/router/index.js
+++ src/router/index.js
@@ -48,11 +48,49 @@ const routes = [
       },
     ]
   },
+  // 模块未开发页面
+  {
+    path: "/undev",
+    name: "undev",
+    component: () =>
+      import(
+        /* webpackChunkName: "undev" */ "@/components/systemErrorPage/undev"
+        ),
+    meta: { title: "该模块未启用" },
+  },
+  // 500页面
+  {
+    path: "/systemError",
+    name: "systemError",
+    component: () =>
+      import(
+        /* webpackChunkName: "systemError" */ "@/components/systemErrorPage/500"
+        ),
+    meta: { title: "服务器内部异常" },
+  },
+  // 404页面
+  {
+    path: "*",
+    name: "notfound",
+    component: () =>
+      import(
+        /* webpackChunkName: "notfound" */ "@/components/systemErrorPage/404"
+        ),
+    meta: { title: "404" },
+  },
 ]
 
-const router = new VueRouter({
+const customRouter = new VueRouter({
   mode: 'hash',
   routes
 })
+
+customRouter.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  next();
+});
 
 export default router
